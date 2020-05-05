4117ae54c0a7c46aea70c9d2b25eec11cf6a315f
Nazim Rizvic <nazim.rizvic@me.com>
2020-05-05 16:55:05 +1000

    created an array that contains the index of letters a to z, based on user message

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,14 +1,24 @@
-a_to_z_lower = ['a','b','c','d','e','f','g',
-            'h','i','j','k','l','m','n',
-            'o','p','q','r','s','t','u',
-            'v','w','x','y','z']
 
-a_to_z_upper = ['A','B','C','D','E','F','G',
-            'H','I','J','K','L','M','N',
-            'O','P','Q','R','S','T','U',
-            'V','W','X','Y','Z']
+def cipher(message,shift)
+
+  a_to_z_lower = ['a','b','c','d','e','f','g',
+              'h','i','j','k','l','m','n',
+              'o','p','q','r','s','t','u',
+              'v','w','x','y','z']
+
+  a_to_z_upper = ['A','B','C','D','E','F','G',
+              'H','I','J','K','L','M','N',
+              'O','P','Q','R','S','T','U',
+              'V','W','X','Y','Z']
+
+  message_array = message.chars       #splits the user input into an array
+
+for letter in message_array do
+  scrambled_message = a_to_z_lower.find_index(letter)
+  p scrambled_message
+end
 
 
+end
 
-puts a_to_z_lower[2]
-puts a_to_z_upper[4]
+cipher("hello! guv?",1)
