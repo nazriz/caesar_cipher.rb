b56aad219e0d58a4f2e2539d0532d9d6baf9d494
Nazim Rizvic <nazim.rizvic@me.com>
2020-05-05 16:30:56 +1000

    Made two seperate arrays. One with lowercase letters one with uppercase letters.

---
+++ caesar_cipher.rb
@@ -0,0 +1,14 @@
+a_to_z_lower = ['a','b','c','d','e','f','g',
+            'h','i','j','k','l','m','n',
+            'o','p','q','r','s','t','u',
+            'v','w','x','y','z']
+
+a_to_z_upper = ['A','B','C','D','E','F','G',
+            'H','I','J','K','L','M','N',
+            'O','P','Q','R','S','T','U',
+            'V','W','X','Y','Z']
+
+
+
+puts a_to_z_lower[2]
+puts a_to_z_upper[4]
