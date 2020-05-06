4ff7f9918559db139b2fdc9ed93aedda901ef230
Nazim Rizvic <nazim.rizvic@me.com>
2020-05-06 15:49:58 +1000

    Completed final functionality of cipher. Can now handle punctuation, spaces and wrapping

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,21 +1,29 @@
 def cipher(message,shift)
 
-  a_to_z_lower = [' ','a','b','c','d','e','f','g',
+  a_to_z = [' ','a','b','c','d','e','f','g',
               'h','i','j','k','l','m','n',
               'o','p','q','r','s','t','u',
-              'v','w','x','y','z']
-
-  a_to_z_upper = ['A','B','C','D','E','F','G',
+              'v','w','x','y','z',
+              'A','B','C','D','E','F','G',
               'H','I','J','K','L','M','N',
               'O','P','Q','R','S','T','U',
-              'V','W','X','Y','Z']
+              'V','W','X','Y','Z','!','@',
+              '#','$','%','^','&','*','(',
+              ')','-','_','+','=',':',';',
+              '?','/','<','>','"',"'"]
 
   message_array = message.chars       #splits the user input into an array
 
 index_array = []
 
 for letter in message_array do
-  index_array.push(a_to_z_lower.find_index(letter))
+
+# if letter == letter.upcase
+#   p letter
+#   index_array.push(a_to_z.find_index(letter))
+# else
+  index_array.push(a_to_z.find_index(letter)) #creates an array from the users
+# end                                                  #input of the index of the letter
 end
 
 
@@ -23,8 +31,10 @@ for index_num in index_array do
 
 if index_num == 0
   scrambled_message = ' '
+elsif index_num < 53
+  scrambled_message = a_to_z[index_num + shift]
 else
-scrambled_message = a_to_z_lower[index_num + shift]
+scrambled_message = a_to_z[index_num]
 end
 
 print scrambled_message
@@ -32,4 +42,4 @@ end
 
 end
 
-cipher("hello world",0)
+cipher("Hello@ '?<>World!", 3)
