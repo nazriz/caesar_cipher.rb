0ef97bb9e97d38408e08683b34c0fa9bba8247c5
Nazim Rizvic <nazim.rizvic@me.com>
2020-05-06 17:17:41 +1000

    Complete rewrite of cipher to fix wrapping bug that was present in previous build. Cipher now functions as expected.

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,13 +1,17 @@
 def cipher(message,shift)
 
-  a_to_z = [' ','a','b','c','d','e','f','g',
+  a_to_z_lower = ['a','b','c','d','e','f','g',
               'h','i','j','k','l','m','n',
               'o','p','q','r','s','t','u',
-              'v','w','x','y','z',
-              'A','B','C','D','E','F','G',
+              'v','w','x','y','z',]
+
+
+  a_to_z_upper =  ['A','B','C','D','E','F','G',
               'H','I','J','K','L','M','N',
               'O','P','Q','R','S','T','U',
-              'V','W','X','Y','Z','!','@',
+              'V','W','X','Y','Z']
+
+  special_char = [' ','!','@',
               '#','$','%','^','&','*','(',
               ')','-','_','+','=',':',';',
               '?','/','<','>','"',"'"]
@@ -16,41 +20,64 @@ def cipher(message,shift)
 
 index_array = []
 
+scrambled_message = ""
+
 for letter in message_array do
 
-# if letter == letter.upcase
-#   p letter
-#   index_array.push(a_to_z.find_index(letter))
-# else
-  index_array.push(a_to_z.find_index(letter)) #creates an array from the users
-# end                                                  #input of the index of the letter
-end
+if special_char.include?(letter)
+temp_char = special_char.find_index(letter)
+scrambled_message += special_char[temp_char]
 
-
-for index_num in index_array do
-
-if index_num == 0
-  scrambled_message = ' '
-elsif index_num < 53
-  scrambled_message = a_to_z[index_num + shift]
+elsif a_to_z_upper.include?(letter)
+  temp_char = a_to_z_upper.find_index(letter) + shift
+if temp_char > 25
+    temp_char = temp_char - 26
+      scrambled_message += a_to_z_upper[temp_char]
 else
-scrambled_message = a_to_z[index_num]
+  scrambled_message += a_to_z_upper[temp_char]
 end
 
-print scrambled_message
-end
+else
+
+    temp_char = a_to_z_lower.find_index(letter) + shift
+  if temp_char > 25
+      temp_char = temp_char - 26
+        scrambled_message += a_to_z_lower[temp_char]
+  else
+    scrambled_message += a_to_z_lower[temp_char]
+  end
 
 end
+end
+p scrambled_message
+end
 
-puts "Ello luv, type ya message innit:"
-print "> "
-message = gets.chomp
-
-puts "And ow' much ya wanna scramble it?"
-print "> "
-scramble = gets.chomp.to_i
-
-puts "Orite, scrambling now..."
-cipher(message,scramble)
-
-puts "There it is."
+cipher("What a string!",5)
+#
+# for index_num in index_array do
+#
+# if index_num == 0
+#   scrambled_message = ' '
+# elsif index_num < 53
+#   scrambled_message = a_to_z[index_num + shift]
+# else
+# scrambled_message = a_to_z[index_num]
+# end
+#
+# print scrambled_message
+# end
+#
+# end
+#
+# puts "Ello luv, type ya message innit:"
+# print "> "
+# message = gets.chomp
+#
+# puts "And ow' much ya wanna scramble it?"
+# print "> "
+# scramble = gets.chomp.to_i
+#
+# puts "Orite, scrambling now...\n"
+# cipher(message,scramble)
+#
+# puts "There it is."
