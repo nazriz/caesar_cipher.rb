be161153357ad5c672ed1b655c8127fa010e7ea8
Nazim Rizvic <nazim.rizvic@me.com>
2020-05-06 17:24:21 +1000

    Commented code. Final commit for project completion.

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -16,6 +16,7 @@ def cipher(message,shift)
               ')','-','_','+','=',':',';',
               '?','/','<','>','"',"'"]
 
+
   message_array = message.chars       #splits the user input into an array
 
 index_array = []
@@ -24,13 +25,13 @@ scrambled_message = ""
 
 for letter in message_array do
 
-if special_char.include?(letter)
-temp_char = special_char.find_index(letter)
+if special_char.include?(letter) #checking for special characters
+temp_char = special_char.find_index(letter) #temp_char is set to the relevant index from the array
 scrambled_message += special_char[temp_char]
 
-elsif a_to_z_upper.include?(letter)
+elsif a_to_z_upper.include?(letter) #checking for uppercase characters
   temp_char = a_to_z_upper.find_index(letter) + shift
-if temp_char > 25
+if temp_char > 25 #mechanism that allows for 'wrapping'. i.e Z wrapped + 1 becomes A and so on.
     temp_char = temp_char - 26
       scrambled_message += a_to_z_upper[temp_char]
 else
@@ -39,7 +40,7 @@ end
 
 else
 
-    temp_char = a_to_z_lower.find_index(letter) + shift
+    temp_char = a_to_z_lower.find_index(letter) + shift #assumes that anything left must be lowercase
   if temp_char > 25
       temp_char = temp_char - 26
         scrambled_message += a_to_z_lower[temp_char]
